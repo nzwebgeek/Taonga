ed94fb7d6bf57b072c8e1ff78fe3fb262c93e128
Mikey Web Geek <93967655+nzwebgeek@users.noreply.github.com>
2023-09-12 12:48:46 +1200

    Update connect.php

--- public/connect.php
+++ public/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host='localhost';
-    $username='root';
-    $password='';
-    $dbname = "aqua";
+    $username='****';
+    $password='****';
+    $dbname = "****";
     $conn=mysqli_connect($host,$username,$password,"$dbname");
    /* if(!$conn)
         {
